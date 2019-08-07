3a3ad4f021da35389b9b306fd10b0cd660aa7016
m4-fitzgerald <martyn2.fitzgerald@live.uwe.ac.uk>
2019-08-07 19:32:29 +0100

    Adding Get Json

--- routes/parking.js
+++ routes/parking.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var request = require("request");
 var router = express.Router();
 
 /* GET parking listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var lat = req.query.lat;
+  var long = req.query.long;
+  var radius = req.query.radius;
+
+  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+    "type=parking" +
+    "&radius=" + radius +
+    "&location=" + lat + "," + long +
+    "&key=AIzaSyBeMKzk8ZpyU2Hk_lrVmlO-Ggq1tQqtYsM"
+
+  request({url: url, json: true}, 
+    function (error, response, body) {
+      if (!error && response.statusCode === 200) 
+      {
+        // Print the json response
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(body, null, 4));
+      }
+    }
+  )
+
 });
 
 module.exports = router;

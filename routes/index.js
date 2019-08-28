d9a79fc75d80476ef26e03a8d14885d38b32e0b1
m4-fitzgerald <martyn2.fitzgerald@live.uwe.ac.uk>
2019-08-28 01:17:59 +0100

    Created Controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var carparkController = require('../controllers/carparks');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Parking Application' });
 });
+
+/* GET parking listing. */
+router.get('/parking/carparks', carparkController.carparks_insert_all);
+
+/* GET parking listing. */
+router.get('/parking', carparkController.carparks_get_all);
+
 
 module.exports = router;

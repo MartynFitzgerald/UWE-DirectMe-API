f5b115a88f93e70f1f01b06390a8c2cda78b8e4b
Martyn Fitzgerald <Martyn2.Fitzgerald@live.uwe.ac.uk>
2020-04-07 18:55:40 +0100

    Cleaned Up Files

---
+++ Model/crud_functionality.js
@@ -0,0 +1,132 @@
+/*=============================================================================
+|      Editors:  Martyn Fitzgerald - 16025948
+|
+|  Module Code:  UFCFR4-45-3
+| Module Title:  Computing Project
+|
+|   Instructor:  Paul Raynor
+|     Due Date:  30/03/2019
+|
+|    File Name:  carparks.js  
+|  Description:  This is the logic behind working out which carparks are within
+|                their area they are searching for.
+*===========================================================================*/
+var dbController = require('../controllers/dbconnection');
+
+/* 
+  Scraping Locations
+*/
+exports.scraping_location_get_all = function(req, res, next) {
+  fetch(`SELECT * FROM scraping_location`, req, res, next);
+}
+
+exports.scraping_location_get_by_id = function(req, res, next) {
+  fetch(`SELECT * FROM scraping_location WHERE scraping_location_id='${req.param('id')}'`, req, res, next);
+}
+
+exports.scraping_location_insert = function(req, res, next) {
+  insert(`INSERT IGNORE INTO scraping_location (name, latitude, longitude, radius) VALUES ('${req.body.name}', '${req.body.latitude}', '${req.body.longitude}', '${req.body.radius}');`, req, res, next);
+}
+
+exports.scraping_location_delete_by_id = function(req, res, next) {
+  del(`DELETE FROM scraping_location WHERE scraping_location_id='${req.param('id')}'`, req, res, next);
+}
+
+/* 
+  Car Parks
+*/
+exports.car_park_get_from_user_location  = function(req, res, next) {
+  /* 
+      Find the car parks within the distance from the users Point
+      https://stackoverflow.com/questions/29916341/geo-location-radius-search-using-php-and-mysql
+  */
+  var sql = `SELECT *, (SQRT(POW(69.1 * (latitude - '${req.query.latitude}'), 2) + POW(69.1 * ('${req.query.longitude}' - longitude) * COS(latitude / 57.3), 2)) * 1609.344) AS distance
+    FROM car_park HAVING distance < '${req.query.radius}' ORDER BY distance`;
+
+  fetch(sql, req, res, next);
+}
+
+exports.car_park_get_all = function(req, res, next) {
+  fetch(`SELECT * FROM car_park`, req, res, next);
+}
+
+exports.car_park_get_by_id = function(req, res, next) {
+  fetch(`SELECT * FROM car_park WHERE car_park_id='${req.param('id')}'`, req, res, next);
+}
+
+exports.car_park_insert = function(req, res, next) {
+  insert(`INSERT IGNORE INTO car_park (name, address, latitude, longitude, last_updated_at, scraping_location_id, external_provider_id) VALUES ('${ req.body.name}',  '${req.body.address}', '${req.body.latitude}', '${req.body.longitude}', '${req.body.last_updated_at}', '${req.body.scraping_location_id}', '${req.body.external_provider_id}');`, req, res, next);
+}
+
+exports.car_park_delete_by_id = function(req, res, next) {
+  del(`DELETE FROM car_park WHERE car_park_id='${req.param('id')}'`, req, res, next);
+}
+
+/* 
+  Users
+*/
+exports.user_get_all = function(req, res, next) {
+  fetch(`SELECT * FROM user;`, req, res, next);
+}
+
+exports.user_get_by_email_address = function(req, res, next) {
+  fetch(`SELECT * FROM user WHERE email_address='${req.param('emailAddress')}';`, req, res, next);
+}
+
+exports.user_insert = function(req, res, next) {
+  insert(`INSERT IGNORE INTO user (fName, lName, email_address, password, phone_number, darkmode, radius) VALUES ('${req.body.fName}', '${req.body.lName}', '${req.body.email_address}', '${req.body.password}', '${req.body.phone_number}', '${req.body.darkmode}', '${req.body.radius}');`, req, res, next);
+}
+
+exports.user_delete_by_email_address = function(req, res, next) {
+  del(`DELETE FROM user WHERE id='${req.param('emailAddress')}'`, req, res, next);
+}
+
+function fetch(sql, req, res, next) {
+  dbController.connection.query(sql, function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    else if (results) {
+      res.status(200).json({
+        result: results
+      });
+    }
+    else if (!results) {
+      res.status(200).json({
+        result: "404 - Error Encountered"
+      });
+    }
+  });
+}
+
+function insert(sql, req, res, next) {
+  dbController.connection.query(sql, function (error, results, fields) {
+    if (results.affectedRows > 0) {
+      res.status(200).json({
+        result: results
+      });
+    }
+    else if (results.affectedRows <= 0) {
+      res.status(204);
+    }
+    else if (error) {
+      throw error;
+    }
+  });
+}
+
+ function del(sql, req, res, next) {
+  dbController.connection.query(sql, function (error, results, fields) {
+    if (results.affectedRows> 0) {
+      res.status(200).json({
+        result: results
+      });
+    }
+    else if (results.affectedRows <= 0) {
+      res.status(204);
+    }
+    else if (error){ 
+      throw error;
+    }
+  });
+}
--- controllers/routes.js
+++ controllers/routes.js
@@ -15,49 +15,60 @@ var express = require('express');
 var router = express.Router();
 
 /* Importing functions from other files */
-var carparkController = require('./carpark');
-var scrapingLocationController = require('./scrapingLocation');
-var userController = require('./user');
+var controller = require('../Model/crud_functionality');
 
 /* Creating a index page for the applicaiton */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Parking Application' });
+  res.render('index', { title: 'DirectMe - API' });
 });
-
-/* 
-  Car Parks
-*/
-/* GET relevant car parks from the db. */
-router.get('/API/GET/CARPARK', carparkController.get_from_user_location);
-
-/* GET all car parks from the db. */
-router.get('/API/GET/CARPARKS/', carparkController.get_all);
-
-/* GET car parks from the db. */
-router.get('/API/GET/CARPARK/:id', carparkController.get_by_id);
 
 /* 
   Scraping Locations
 */
 /* GET All Scraping Locations from the db. */
-router.get('/API/GET/SCRAPINGLOCATIONS/', scrapingLocationController.get_all);
+router.get('/API/GET/SCRAPINGLOCATIONS/', controller.scraping_location_get_all);
 
 /* GET Scrapings Location from the db. */
-router.get('/API/GET/SCRAPINGLOCATIONS/:id', scrapingLocationController.get_by_id);
+router.get('/API/GET/SCRAPINGLOCATION/:id', controller.scraping_location_get_by_id);
 
 /* INSERT Scrapings Location into the db. */
-router.post('/API/INSERT/SCRAPINGLOCATIONS/', scrapingLocationController.insert);
+router.post('/API/INSERT/SCRAPINGLOCATION/', controller.scraping_location_insert);
 
 /* DELETE Scrapings Location from the db. */
-router.delete('/API/DELETE/SCRAPINGLOCATIONS/:id', scrapingLocationController.delete_by_id);
+router.delete('/API/DEL/SCRAPINGLOCATION/:id', controller.scraping_location_delete_by_id);
+
+/* 
+  Car Parks
+*/
+/* GET relevant car parks from the db. */
+router.get('/API/GET/CARPARK', controller.car_park_get_from_user_location);
+
+/* GET all car parks from the db. */
+router.get('/API/GET/CARPARKS/', controller.car_park_get_all);
+
+/* GET car parks from the db. */
+router.get('/API/GET/CARPARK/:id', controller.car_park_get_by_id);
+
+/* INSERT car parks into the db. */
+router.post('/API/INSERT/CARPARK/', controller.car_park_insert);
+
+/* DELETE car parks from the db. */
+router.delete('/API/DEL/CARPARK/:id', controller.car_park_delete_by_id);
+
 
 /* 
   Users
 */
 /* GET car parks from the db. */
-router.get('/API/GET/USERS/', userController.get_all);
+router.get('/API/GET/USERS/', controller.user_get_all);
 
 /* GET car parks from the db. */
-router.get('/API/GET/USER/:emailAddress', userController.get_by_email_address);
+router.get('/API/GET/USER/:emailAddress', controller.user_get_by_email_address);
+
+/* INSERT car parks into the db. */
+router.post('/API/INSERT/USER/', controller.user_insert);
+
+/* DELETE car parks from the db. */
+router.delete('/API/DEL/USER/:emailAddress', controller.user_delete_by_email_address);
 
 module.exports = router;

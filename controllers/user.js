3fb2b6e3b62cc2e2c25fcdf43949125f12b244a9
Martyn Fitzgerald <martynfitzzz2014@gmail.com>
2020-07-03 19:23:09 +0100

    Added Schema Into User

--- controllers/user.js
+++ controllers/user.js
@@ -23,10 +23,10 @@ exports.get_by_email_address = function(req, res, next) {
   crud.read(`SELECT * FROM user WHERE email_address="${req.param('emailAddress')}";`, req, res, next);
 }
 exports.insert = function(req, res, next) {
-  crud.insert(`INSERT IGNORE INTO user (user_id, fName, lName, email_address, password, phone_number, darkmode, radius, profile_picture) VALUES ("${req.body.user_id}","${req.body.fName}", "${req.body.lName}", "${req.body.email_address}", "${req.body.password}", "${req.body.phone_number}", 0, 1500, "${req.body.profile_picture}");`, req, res, next);
+  crud.insert(`INSERT IGNORE INTO user (user_id, fName, lName, email_address, password, phone_number, darkmode, radius, profile_picture, scheme) VALUES ("${req.body.user_id}","${req.body.fName}", "${req.body.lName}", "${req.body.email_address}", "${req.body.password}", "${req.body.phone_number}", "${req.body.darkmode}", "${req.body.radius}", "${req.body.profile_picture}", "${req.body.scheme}");`, req, res, next);
 }
 exports.update_by_id = function(req, res, next) {
-  crud.update(`UPDATE user SET fName="${req.body.fName}", lName="${req.body.lName}", email_address="${req.body.email_address}", password="${req.body.password}", phone_number="${req.body.phone_number}", darkmode=${req.body.darkmode}, radius=${req.body.radius}, profile_picture="${req.body.profile_picture}" WHERE user_id="${req.body.user_id}";`, req, res, next);
+  crud.update(`UPDATE user SET fName="${req.body.fName}", lName="${req.body.lName}", email_address="${req.body.email_address}", password="${req.body.password}", phone_number="${req.body.phone_number}", darkmode=${req.body.darkmode}, radius=${req.body.radius}, profile_picture="${req.body.profile_picture}", scheme="${req.body.scheme}" WHERE user_id="${req.body.user_id}";`, req, res, next);
 }
 exports.delete_by_id = function(req, res, next) {
   crud.del(`DELETE FROM user WHERE user_id="${req.body.user_id}"`, req, res, next);
